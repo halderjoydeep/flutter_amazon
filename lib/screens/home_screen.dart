c466cce22fc43642492faad22def04c9a4977ef7
halderjoydeep <halderjoydeep1997@gmail.com>
2021-06-09 01:07:40 +0530

    rough design for homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Amazon"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.phone_android),
+                title: Text("List Item"),
+                trailing: Icon(CupertinoIcons.cart_badge_plus),
+              ),
+            ),
+          );
+        },
+      ),
+      drawer: Drawer(),
+    );
   }
 }
